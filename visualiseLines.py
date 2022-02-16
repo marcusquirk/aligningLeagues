3d99c8ceb5409ef4783283be6296d98dac0f3dae
marcusquirk <marcus.quirk@gmail.com>
2022-02-16 10:59:26 -0600

    Added getSides.py file a function to get the subsets of points from a dividing line

--- visualiseLines.py
+++ visualiseLines.py
@@ -1,6 +1,5 @@
 from findConvexHull import findConvexHull
-from tupleSubtract import tupleSubtract
-import numpy as np
+from getSides import getSides
 from distanceFunction import distanceFunction
 import math
 
@@ -25,37 +24,6 @@ def visualiseLines(line, left, right, plt, limits, extent=None, backgroundMap=No
     ax.set_xlim(limits[0], limits[1])
     ax.set_ylim(limits[2], limits[3])
     plt.show()
-
-
-def getSides(line, x, y, names):
-    left = []
-    right = []
-    middle = []
-
-    vector = tupleSubtract(line[1], line[0])
-    for i in range(len(x)):
-        point = (x[i], y[i])
-        vector2 = tupleSubtract(point, line[0])
-        crossProduct = np.cross(vector, vector2)
-        if crossProduct > 0.000001:
-            # Points are reversed so they can be passed as lat/long to the distance function
-            left.append([point[::-1], names[i]])
-        elif crossProduct < -0.000001:
-            right.append([point[::-1], names[i]])
-        else:
-            middle.append([point[::-1], names[i]])
-
-    counter = 0
-    while len(right) < len(x) // 2:
-        right.append(middle[counter])
-        counter += 1
-    while len(left) < len(x) // 2:
-        left.append(middle[counter])
-        counter += 1
-    if counter < len(middle):
-        left.append(middle[counter])
-
-    return left, right
 
 
 def findBestLines(dividingLines, plt, x, y, limits, gamesStructure, extent=None, backgroundMap=None, names=None):
